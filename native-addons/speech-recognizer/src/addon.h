903edb8ef2c91bb51727f58b179b2fbd9abdac08
郑嘉乐 <416510841@qq.com>
2025-09-16 04:54:14 +0800

    Update addon.h

--- native-addons/speech-recognizer/src/addon.h
+++ native-addons/speech-recognizer/src/addon.h
@@ -1,4 +1,4 @@
-// 文件：native-addons/speech-recognizer/src/addon.h (修正版)
+// 文件：native-addons/speech-recognizer/src/addon.h (最终版)
 #pragma once
 #include <napi.h>
 
@@ -8,18 +8,16 @@ public:
     SpeechRecognizerWrapper(const Napi::CallbackInfo& info);
     ~SpeechRecognizerWrapper();
 
-    // 【修正】将静态方法声明移到 public 区域
     static Napi::Value RequestAuthorization(const Napi::CallbackInfo& info);
 
 private:
-    // 实例方法保持 private
     void Start(const Napi::CallbackInfo& info);
     void Stop(const Napi::CallbackInfo& info);
 
-    // Objective-C/Swift 对象的指针
-    id swiftRecognizer;
+    // 【加回来】将 constructor 声明为私有静态成员
+    static Napi::FunctionReference constructor;
 
-    // 线程安全的 JS 回调函数
+    id swiftRecognizer;
     Napi::ThreadSafeFunction onResultCallback;
     Napi::ThreadSafeFunction onErrorCallback;
-}; // 确保这里只有一个结束花括号
+};
